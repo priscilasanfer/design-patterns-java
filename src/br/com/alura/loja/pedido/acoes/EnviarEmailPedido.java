cc026b32b995e4a5c34e7b34aa45df9301d1484b
Priscila Ferreira <priscilasanfer@gmail.com>
2021-07-10 09:13:51 -0300

    Refatorando a classe GeraPedidoHandler sem design pattern

---
+++ src/br/com/alura/loja/pedido/acoes/EnviarEmailPedido.java
@@ -0,0 +1,9 @@
+package br.com.alura.loja.pedido.acoes;
+
+import br.com.alura.loja.pedido.Pedido;
+
+public class EnviarEmailPedido {
+    public void executar(Pedido pedido){
+        System.out.println("Enviar email pedido");
+    }
+}
---
+++ src/br/com/alura/loja/pedido/acoes/SalvarPedidoNoBanco.java
@@ -0,0 +1,9 @@
+package br.com.alura.loja.pedido.acoes;
+
+import br.com.alura.loja.pedido.Pedido;
+
+public class SalvarPedidoNoBanco {
+    public void executar(Pedido pedido) {
+        System.out.println("Salvando pedido no banco de dados");
+    }
+}
